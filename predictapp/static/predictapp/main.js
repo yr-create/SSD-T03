685b24f077e10f1d8954fd595b54066a6835227e
djangouser <user@wxample.com>
2023-07-18 11:56:07 +0900

    last

--- predictapp/static/predictapp/main.js
+++ predictapp/static/predictapp/main.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const url = "/static/predictapp/Json/2023-07.json";	// JSONファイル名
-=======
-const url = "/static/predictapp/Json/2015-05.json";	// JSONファイル名
->>>>>>> 7878fb4e933db6b56bb7fac4e1b48542aca23c17
 let result;
 
 // JSONファイルを整形して表示する
@@ -10,26 +6,16 @@ function formatJSON(data){
 	// 整形して表示
 	let html = "<table>";
 	html += "<caption>" + data.caption + "</caption>";
-<<<<<<< HEAD
 	
-=======
-
->>>>>>> 7878fb4e933db6b56bb7fac4e1b48542aca23c17
 	for(let member of data.members){
 		html += "<tr><td>" + String(member.id) + "</td><td>"+ member.name + "</td><td>" + String(member.sales) + "</td></tr>";
 	}
 	html += "</table>";
 
-<<<<<<< HEAD
 	html += "<p>ぜんぶで" + data.members.length + "種類" + "</p>";	// 要素memberの配列要素数
 
 	result.innerHTML = html;
 	
-=======
-	//html += "<p>ぜんぶで" + data.members.length + "種類" + "</p>";	// 要素memberの配列要素数
-
-	result.innerHTML = html;
->>>>>>> 7878fb4e933db6b56bb7fac4e1b48542aca23c17
 }
 
 // 起動時の処理
